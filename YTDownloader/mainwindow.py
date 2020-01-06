5cfb395b9af4d0f23462008359918c37a8d7bb22
Sascha-Koejer <koejer.sascha@gmail.com>
2020-01-06 20:31:44 +0100

    Version 1.0

---
+++ YTDownloader/mainwindow.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'YTDownloader\mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(370, 162)
+        MainWindow.setMinimumSize(QtCore.QSize(370, 162))
+        MainWindow.setMaximumSize(QtCore.QSize(370, 162))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.urlinput = QtWidgets.QLineEdit(self.centralwidget)
+        self.urlinput.setObjectName("urlinput")
+        self.horizontalLayout.addWidget(self.urlinput)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 1, 1, 2)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setObjectName("progressBar")
+        self.horizontalLayout_2.addWidget(self.progressBar)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 2, 1, 1, 2)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.checkBox.setTristate(False)
+        self.checkBox.setObjectName("checkBox")
+        self.horizontalLayout_3.addWidget(self.checkBox)
+        self.addbutton = QtWidgets.QPushButton(self.centralwidget)
+        self.addbutton.setObjectName("addbutton")
+        self.horizontalLayout_3.addWidget(self.addbutton)
+        self.downloadbutton = QtWidgets.QPushButton(self.centralwidget)
+        self.downloadbutton.setObjectName("downloadbutton")
+        self.horizontalLayout_3.addWidget(self.downloadbutton)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 1, 1, 1, 2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 370, 22))
+        self.menuBar.setObjectName("menuBar")
+        self.menuDatei = QtWidgets.QMenu(self.menuBar)
+        self.menuDatei.setObjectName("menuDatei")
+        MainWindow.setMenuBar(self.menuBar)
+        self.action_download_path = QtWidgets.QAction(MainWindow)
+        self.action_download_path.setObjectName("action_download_path")
+        self.menuDatei.addAction(self.action_download_path)
+        self.menuBar.addAction(self.menuDatei.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "YouTube Downloader"))
+        self.urlinput.setPlaceholderText(_translate("MainWindow", "YouTube Url"))
+        self.checkBox.setText(_translate("MainWindow", "Video"))
+        self.addbutton.setText(_translate("MainWindow", "Add"))
+        self.downloadbutton.setText(_translate("MainWindow", "Download All"))
+        self.menuDatei.setTitle(_translate("MainWindow", "Datei"))
+        self.action_download_path.setText(_translate("MainWindow", "Dowload Pfad"))
---
+++ main.py
@@ -0,0 +1,96 @@
+import sys
+import os
+import queue
+from pytube import YouTube
+from configparser import  ConfigParser
+from qtpy import QtWidgets
+from YTDownloader.mainwindow import Ui_MainWindow
+from PyQt5.QtCore import QThread, pyqtSignal
+
+
+settings_folder = os.path.expandvars(R"C:\Users\$USERNAME\Documents\YTDownloader")
+settings_path = os.path.expandvars(R"C:\Users\$USERNAME\Documents\YTDownloader\settings.ini")
+config = ConfigParser()
+q = queue.PriorityQueue()
+
+if os.path.isfile(settings_path) != True:
+    config["Settings"] = {
+        "path" : "."
+    }
+    if os.path.exists(settings_folder) != True:
+        os.mkdir(os.path.expandvars(settings_folder))
+    with open(settings_path, "w") as configfile:
+        config.write(configfile)
+
+config.read(settings_path)
+
+app = QtWidgets.QApplication(sys.argv)
+max_file_size = 0
+
+class MainWindow(QtWidgets.QMainWindow):
+    urlChanged = pyqtSignal(str)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.action_download_path.triggered.connect(self.change_dir)
+        self.ui.progressBar.setMaximum(1)
+
+        self.ui.addbutton.clicked.connect(self.add_waitinglist)
+        self.ui.downloadbutton.clicked.connect(self.start_download)
+        
+    def add_waitinglist(self):
+        url = self.ui.urlinput.text()
+        qualtiy = 140
+        if self.ui.checkBox.isChecked():
+            qualtiy = 18
+
+        self.ui.urlinput.clear()
+        q.put((qualtiy, url))
+        
+    def start_download(self):
+        self.calc = Download()
+        self.calc.valueChanged.connect(self.change_progress)
+        self.calc.start()
+        
+    def change_progress(self, value):
+        self.ui.progressBar.setMinimum(-max_file_size)
+        if value == 0:
+            self.ui.urlinput.clear()
+            self.ui.progressBar.setValue(1)
+        self.ui.progressBar.setValue(-value)
+        
+    def change_dir(self):
+        dir_ = QtWidgets.QFileDialog.getExistingDirectory(None, 'Wähle einen Ordner:', config.get('Settings', 'path'), QtWidgets.QFileDialog.ShowDirsOnly)
+        config.set("Settings", "path", dir_)
+        with open(settings_path, "w") as configfile:
+            config.write(configfile)
+
+class Download(QThread):
+
+    valueChanged = pyqtSignal(int)
+
+    def run(self, *params):
+        try:
+            while not q.empty():
+                video = q.get()
+                print(video)
+                yt = YouTube(video[1], on_progress_callback = self.in_progress, on_complete_callback = self.run)
+                stream = yt.streams.get_by_itag(video[0])
+
+                global max_file_size
+                max_file_size = stream.filesize
+                stream.download(config.get('Settings', 'path'))
+
+                self.valueChanged.emit(0)
+
+        except BaseException as error:
+            print('An exception occurred: {}'.format(error))
+
+    def in_progress(self, stream, unknown, buffer, bytes_left):
+        self.valueChanged.emit(bytes_left)
+
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec_())
